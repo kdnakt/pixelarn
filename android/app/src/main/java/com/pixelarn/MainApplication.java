30c709df50a71e248a265a5f877d8f7e69222568
kdnakt <a.kid.1985@gmail.com>
2019-01-19 06:51:29 +0900

    link realm native modules

--- android/app/src/main/java/com/pixelarn/MainApplication.java
+++ android/app/src/main/java/com/pixelarn/MainApplication.java
@@ -3,6 +3,7 @@ package com.pixelarn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new VectorIconsPackage(),
             new SvgPackage()
       );
