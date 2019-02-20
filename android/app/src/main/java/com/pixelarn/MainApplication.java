fb9f47dcb71b81dccc5c2ebc372d3a23cb0cf067
kdnakt <a.kid.1985@gmail.com>
2019-02-21 07:19:18 +0900

    move realm from devDependencies to dependencies

--- android/app/src/main/java/com/pixelarn/MainApplication.java
+++ android/app/src/main/java/com/pixelarn/MainApplication.java
@@ -3,6 +3,8 @@ package com.pixelarn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import io.realm.react.RealmReactPackage;
@@ -28,6 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
+            new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
             new RealmReactPackage(),
